125da551299c19cd72990008adac62df2c171ea7
Junheon Lee <junheon@outlook.com>
2013-08-18 15:22:35 +0900

    Initial commit

---
+++ mission07/expert/junheon/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Junheon::Application.config.secret_key_base = '13d71d31dea264e18ee7899216abbd8cae69b0365c45c5245abb11682dd2d027d0653706b20db26bcc801fc57fd4a0ea7772b4a141fa99087ea6c0bd5ab4e95c'
---
+++ mission07/expert/junheon/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Junheon::Application.config.session_store :cookie_store, key: '_junheon_session'
