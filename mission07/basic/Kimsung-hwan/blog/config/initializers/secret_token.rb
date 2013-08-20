885b4ee706701d8037f44ef440744cd9b0dc7030
Kimsung-hwan <ssshwan@gmail.com>
2013-08-20 14:57:34 +0000

    Kimsung-hwan mission7 complete

---
+++ mission07/basic/Kimsung-hwan/blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'dff9a27c4de3eab9ce66dfb556e0e0a63bbeded8726c98b58a7b7b90ed107cc6eaaee06740e3d4e080cae0efb15eb9b059b62c3056ccde1b37cdc41745ba4139'
