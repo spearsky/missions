4bbd748adff39d9e8a69f606922deaaffab1dc8b
spearsky <your.spearsky@gmail.com>
2013-08-20 17:23:09 +0900

    initial commit

---
+++ mission07/basic/eb.lee/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EbLee::Application.config.secret_key_base = 'e3343cfff800bf1993ca4cd24b28b2016d19cb0981d4294ae72d681cf6b6744742959144961a65c75d7117c75cd9a8bda08bd4db4dede24c1a01856744979599'
