d988fb81b134cd5ae543eb82c1171cb35ebe4770
Insomous <DemianHwang@gmail.com>
2013-08-20 14:21:30 +0000

    황인성 Mission7

---
+++ mission07/basic/insomous/blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '5610a8c1c123f36b8170c9e7ca55a9e9e365849647bb37c6c795f52151507cbef6c297edd953b837f323b16279e2f7bc2d9b0e42225ef36137be6385b25c6ca5'
