83b85ee9cd11dc411bb150c30405304be75e3488
Kim Daekwon <propellerheaven@gmail.com>
2013-08-21 15:19:04 +0900

    rm nacyot

--- mission07/expert/nacyot/config/boot.rb
+++
@@ -1,5 +0,0 @@
-# Set up gems listed in the Gemfile.
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-ENV["SKIP_RAILS_ADMIN_INITIALIZER"] = "false"
-
-require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
