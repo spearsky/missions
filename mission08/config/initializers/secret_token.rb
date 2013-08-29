84b841d44f0839ac7785908478b4e96d4928735c
spearsky <your.spearsky@gmail.com>
2013-08-29 15:46:23 +0900

    first

---
+++ mission08/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mission08::Application.config.secret_key_base = '6ef2a2123c840a8dc78f03f25c9fcaa01ce2dbce5b934ee2e5774c7cf675dda5f7f1dacf4431d0501d6f7faee684808d7ed27c3c6948ba8e3658323bef91881e'
---
+++ mission08/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mission08::Application.config.session_store :cookie_store, key: '_mission08_session'
